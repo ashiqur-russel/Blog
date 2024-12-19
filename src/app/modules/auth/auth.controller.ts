f16c453444abd77fc80db7e0034e4aca1c6ff4d9
ashiqur-russel <ashiqurrussel2@gmail.com>
2024-12-19 19:05:34 +0100

    Create generic response message

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,16 +1,19 @@
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/response';
 import { AuthServices } from './auth.service';
 import { Request, Response } from 'express';
 
-const register = async (req: Request, res: Response) => {
-  console.log('register', req.body);
-  const result = await AuthServices.registerUser(req.body);
-
-  res.status(201).json({
-    message: 'User created successfully',
-    status: true,
-    data: result,
-  });
-};
+const register = catchAsync(async (req: Request, res: Response) => {
+    
+    const result = await AuthServices.registerUser(req.body);
+  
+    sendResponse(res, {
+      statusCode: 201,
+      success: true,
+      message: 'User registered successfully',
+      data: { _id: result._id ,name: result.name, email: result.email },
+    });
+  })
 
 export const AuthControllers = {
   register,
