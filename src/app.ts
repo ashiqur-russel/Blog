58d76aada91475daf8cc6e4cc46ae475060584c9
ashiqur-russel <ashiqurrussel2@gmail.com>
2024-12-28 23:33:05 +0100

    allow origin

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,20 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
+const allowedOrigins = ['http://localhost:3000', 'https://blog-psi-two-74.vercel.app'];
+
+
 //parsers
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins, // Allow specific origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true, // Allow cookies
+  })
+);
+
 
 // application routes
 app.use('/api', router);
